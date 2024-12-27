571bfed2120d4c8b9daab56648e6750ebb56a9a6
Jordan <jordan.miranda@tecnogroup.com.br>
2024-12-26 23:20:16 -0300

    Videos

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implementar ouvintes de eventos do node aqui
     },
+    video: true, // Habilita a gravação de vídeos
+    videoCompression: 32, // Ajusta a compressão do vídeo (0-51, onde 0 é sem compressão)
+    videosFolder: 'cypress/videos', // Pasta onde os vídeos serão salvos
+    screenshotOnRunFailure: true, // Tira screenshots em caso de falhas
   },
 });
